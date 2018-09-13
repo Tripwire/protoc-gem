82c6b57a95888bcc1773d3e0d8d2cdf6a456e747
Chris Kim <ckim@tripwire.com>
2018-09-13 15:56:47 -0700

    Bumping version #.

--- lib/protoc/version.rb
+++ lib/protoc/version.rb
@@ -2,5 +2,5 @@ module Protoc
   # The version of the protobuf library from which these protoc binaries were built
   PROTOBUF_VERSION = '2.6.1'
   # This gem's version
-  VERSION = PROTOBUF_VERSION + '.2'
+  VERSION = PROTOBUF_VERSION + '.3'
 end
